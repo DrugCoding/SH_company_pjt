99319fceb197f8ae245878286d0e47d0cf5ecf78
DrugCoding <lhs940308@gmail.com>
2023-03-29 22:09:43 +0900

    chore: performances 에러 수정

--- performances/views.py
+++ performances/views.py
@@ -11,19 +11,20 @@ def index(request):
     return render(request, 'performances/index.html', context)
 
 def create(request):
-    if request.method == 'POST':
-        p_form = PerformanceForm(request.POST)
-        if p_form.is_valid():
-            p_article = p_form.save(commit=False)
-            p_article.user = request.user
-            p_article.save()
-            return redirect('performances:index')
-    else:
-        p_form = PerformanceForm()
-    context = {
-        'p_form': p_form
-    }
-    return render(request, 'performances/create.html', context)
+    if request.user.is_superuser: # 관리자만 작성 가능
+        if request.method == 'POST':
+            p_form = PerformanceForm(request.POST)
+            if p_form.is_valid():
+                p_article = p_form.save(commit=False)
+                p_article.user = request.user
+                p_article.save()
+                return redirect('performances:index')
+        else:
+            p_form = PerformanceForm()
+        context = {
+            'p_form': p_form
+        }
+        return render(request, 'performances/create.html', context)
 
 def detail(request, p_pk):
     p_article = Performance.objects.get(pk=p_pk)
@@ -34,18 +35,19 @@ def detail(request, p_pk):
 
 def update(request, p_pk):
     p_article = Performance.objects.get(pk=p_pk)
-    if request.method == 'POST':
-        p_form = PerformanceForm(request.POST, request.FILES, instance=p_article)
-        if p_form.is_valid():
-            p_form.save()
-            return redirect('performances:detail', p_pk)
-    else:
-        p_form = PerformanceForm(instance=p_article)
-    context = {
-        'p_form': p_form,
-        'p_article': p_article,
-    }
-    return render(request, 'performance/detail', context)
+    if request.user.is_superuser: # 관리자만 수정 가능
+        if request.method == 'POST':
+            p_form = PerformanceForm(request.POST, instance=p_article)
+            if p_form.is_valid():
+                p_form.save()
+                return redirect('performances:detail', p_pk)
+        else:
+            p_form = PerformanceForm(instance=p_article)
+        context = {
+            'p_form': p_form,
+            'p_article': p_article,
+        }
+        return render(request, 'performances/create.html', context)
 
 def delete(request, p_pk):
     p_article = Performance.objects.get(pk=p_pk)
