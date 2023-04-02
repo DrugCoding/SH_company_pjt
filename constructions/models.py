ed488570f352cfa4d88c99a9c2b42f5c1dd6d1e2
DrugCoding <lhs940308@gmail.com>
2023-04-02 17:15:13 +0900

    feat: base.html 등 맵핑

--- constructions/models.py
+++ constructions/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Constructions(models.Model):
-    title = models.CharField(max_length=50) 
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/',blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
